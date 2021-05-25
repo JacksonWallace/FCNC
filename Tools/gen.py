1d73cdd3c231250e07850fae44a1ba0763c226eb
Jackson Wallace <jackson.wallace@gmail.com>
2021-05-25 12:35:34 -0700

    fixed charge function

--- Tools/gen.py
+++ Tools/gen.py
@@ -1,5 +1,25 @@
 import awkward as ak
 
+def get_charge_parent(particle):
+    parent = find_first_parent(particle)
+    charge = ak.zeros_like(parent)
+    one = [[-11, -13, -15, -17, 24, 37], 1]
+    minus_one = [[11, 13, 15, 17, -24, -37], -1]
+    two_thirds = [[2, 4, 6, 8], 2/3]
+    minus_two_thirds = [[-2, -4, -6, -8], -2/3]
+    minus_one_third = [[1, 3, 5, 7], -1/3]
+    one_third = [[-1, -3, -5, -7], 1/3]
+    zero = [[12, 14, 16, 18, 9, 21, 22, 23, 25], 0]
+    
+    charge_pairs = [one, minus_one, two_thirds, minus_two_thirds, minus_one_third, zero]
+    
+    for pair in charge_pairs:
+        for ID in pair[0]:
+            charge = (parent == ID)*ak.ones_like(parent)*pair[1] + (~(parent == ID))*charge
+            
+    return charge
+    
+    
 def find_first_parent(particle, maxgen=10):
     tmp_mother = particle.parent
     out_mother_pdg = tmp_mother.pdgId
@@ -43,22 +63,3 @@ def get_lepton_from(events, parent_pdg=6):
     gp_lep_fromW = gp_lep[abs(gp_lep.parent.pdgId)==24]
     gp_leptonic_W_parent = find_first_parent(gp_lep_fromW.parent, maxgen=19)
     return gp_lep_fromW[gp_leptonic_W_parent==parent_pdg]
-
-def get_charge_parent(particle):
-    parent = find_first_parent(particle)
-    if parent == 11 or parent == 13 or parent == 15 or parent == 17 or parent == -24 or parent == -37:
-        charge = -1
-    elif parent == -11 or parent == -13 or parent == -15 or parent == -17 or parent == 24 or parent == 37:
-        charge = 1
-    elif parent == 2 or parent == 4 or parent == 6 or parent == 8:
-        charge = 2/3
-    elif parent == -2 or parent == -4 or parent == -6 or parent == -8:
-        charge = -2/3
-    elif parent == 1 or parent == 3 or parent == 5 or parent == 7:
-        charge = -1/3
-    elif parent == -1 or parent == -3 or parent == -5 or parent == -7:
-        charge = 1/3
-    elif parent == 12 or parent == 14 or parent == 16 or parent == 18 or parent == 9 or parent == 21 or parent == 22 or parent == 23 or parent == 25:
-        charge = 0
-    return charge
-    
